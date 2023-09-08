79e29c1bcb4fa329d2aa9ee7bd07d407336e548a
Markos Mk <markosmk@me.com>
2023-09-08 00:58:12 -0300

    add: alias typescript routes import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./src'],
+        alias: {
+          '^src/(.+)': './src/\\1',
+          tests: './tests',
+        },
+      },
+    ],
+  ],
 };
