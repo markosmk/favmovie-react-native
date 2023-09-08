cabcb475be1eae12525f0d8ebc317eec0aeaf8a6
Markos Mk <markosmk@me.com>
2023-09-08 01:41:53 -0300

    add: tailwind native wid

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,15 @@
-import {SafeAreaView, Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <View className="flex-1 bg-green-500">
+        <Text className="text-orange-600 font-bold">HomeScreen</Text>
+        <Text className="text-2xl text-black dark:text-white">
+          HomeScreen 2
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
